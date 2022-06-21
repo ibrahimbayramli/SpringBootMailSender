31ca152ecb876fedab60c26930820c6891d9d2a1
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-06-21 14:10:18 +0300

    Spring Boot Mail Sender Application

---
+++ src/main/java/MailSender/MailSenderApplication.java
@@ -0,0 +1,22 @@
+package MailSender;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class MailSenderApplication {
+
+	@Autowired
+	private MailSenderService senderService;
+	public static void main(String[] args) {
+		SpringApplication.run(MailSenderApplication.class, args);
+	}
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail(){
+		senderService.sendMail("testMail@gmail.com","test","test mail");
+	}
+
+}
---
+++ src/main/java/MailSender/MailSenderService.java
@@ -0,0 +1,25 @@
+package MailSender;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSenderService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMail(String toEmail,String subject,String body){
+        SimpleMailMessage message=new SimpleMailMessage();
+        message.setFrom("yourMailAdress@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+
+        System.out.println("Mail sent succesfully.");
+    }
+
+}
